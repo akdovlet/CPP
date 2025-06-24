e86ccc79222cb548e59d48042377711b1d2d2126
akdovlet <akhmed.dovletov@live.fr>
2025-06-24 18:35:36 +0200

    DONE: first exercise

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 17:32:15 by akdovlet          #+#    #+#             */
+/*   Updated: 2025/06/24 18:06:44 by akdovlet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <cctype>
+
+void	megaphone(std::string str)
+{
+	for (int i = 0; str[i]; i++)
+		str[i] = std::toupper(str[i]);
+	std::cout << str;
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; av[i]; i++)
+		megaphone(av[i]);
+	std::cout << std::endl;
+	return (0);
+}
