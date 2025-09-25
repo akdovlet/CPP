460e9b3235189c24c25c5b5071330d04e4040a20
akdovlet <akhmed.dovletov@live.fr>
2025-09-25 10:38:04 +0200

    CHORE: cleanup

--- CPP06/ex00/src/main.cpp
+++ CPP06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 18:18:09 by akdovlet          #+#    #+#             */
-/*   Updated: 2025/09/24 11:16:14 by akdovlet         ###   ########.fr       */
+/*   Updated: 2025/09/25 10:35:24 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int ac, char** av)
 {
 	if (ac > 1)
-	{
 		ScalarConverter::convert(av[1]);
-	}
+	else
+		std::cout << "./convert [value]" << std::endl;
 }
