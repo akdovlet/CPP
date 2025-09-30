be2908c961ba571c0e5913d88f12950df87211f2
akdovlet <akhmed.dovletov@live.fr>
2025-09-30 19:33:48 +0200

    WIP: thoery crafting cpp08 ex02

--- CPP08/ex02/include/MutantStack.hpp
+++ CPP08/ex02/include/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:39:12 by akdovlet          #+#    #+#             */
-/*   Updated: 2025/09/29 19:32:32 by akdovlet         ###   ########.fr       */
+/*   Updated: 2025/09/30 18:47:26 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,40 +29,61 @@ public:
 	private:
 		std::stack<T>* t_ptr;
 	public:
-		typedef std::stack<T>* pointer;
-		typedef std::stack<T>& reference;
-	
-		iterator(std::stack<T>* ptr = NULL) : t_ptr(ptr) {}
+		typedef std::stack<T>	value_type;
+		typedef std::stack<T>*	pointer;
+		typedef std::stack<T>&	reference;
+		typedef std::ptrdiff_t	difference_type;
+		
+		iterator(pointer ptr = NULL) : t_ptr(ptr) {}
 		iterator(const iterator& other) : t_ptr(other.t_ptr) {}
 		~iterator() {}
 	
-		iterator& operator=(const iterator& other) { t_ptr = other.t_ptr; return (*this); }	
+		operator	bool()const { if (t_ptr) {return true;} else { return (false);} }
+		
+		iterator& operator+=(const difference_type& mov) { t_ptr += mov; return (*this); }
+		iterator& operator-=(const difference_type& mov) { t_ptr -=mov; return (*this); }
+		
 		iterator& operator++() { t_ptr++; return *this; }
-		iterator& operator++(int) { T old = *this; ++(*this); return (old); }
 		iterator& operator--() { t_ptr--; return *this; }
-		iterator& operator--(int) { T old = *this; --(*this); return (old); }
-		iterator operator+(const std::ptrdiff_t mov) { pointer oldPtr = t_ptr;}
-		bool	operator==(iterator other) const { return t_ptr == other.t_ptr; }
-		bool	operator!=(iterator other) const { return t_ptr != other.t_ptr; }
+		iterator operator++(int) { iterator old(*this); ++t_ptr; return (old); }
+		iterator operator--(int) { iterator old(*this); --t_ptr; return (old); }
 		
+		iterator operator+(const difference_type mov) { pointer oldPtr = t_ptr; t_ptr += mov; iterator tmp(*this); t_ptr = oldPtr; return (tmp); }
+		iterator operator-(const difference_type mov) { pointer oldPtr = t_ptr; t_ptr -= mov; iterator tmp(*this); t_ptr = oldPtr; return (tmp); }
 		
-		// iterator begin() { return (&top() - size()); }
-		// iterator end() { return (&top() + 1); };
+		difference_type operator-(const iterator& other) { return (std::distance(other.getPointer(), getPointer())) ; }
+		
+		iterator& operator=(const iterator& other) { t_ptr = other.t_ptr; return (*this); }
+		iterator& operator=(iterator *ptr) { t_ptr = ptr; return (*this); }	
+		
+		bool	operator==(const iterator& other) const { return t_ptr == other.getPointer(); }
+		bool	operator!=(const iterator& other) const { return t_ptr != other.getConstPointer(); }
+		
+		reference		operator*() { return (*t_ptr); }
+		// const reference operator*() const { return (*t_ptr); }
+		pointer			operator->() { return (t_ptr); }
+		
+		iterator*	getPointer() { return (t_ptr); }
+		const iterator* getConstPointer() const { return (t_ptr); }
 	};
-
-	MutantStack();
-	~MutantStack();
+	
+	MutantStack() : st() {}
+	MutantStack(const std::stack<T>& other) : std::stack<T>(other) {}
+	MutantStack& operator=(const MutantStack& other) { /*stack::operator=(other);*/(void)other; return (*this); }
+	~MutantStack() {}
+private:
+	std::stack<T> st;
 };
 
-template<typename T>
-MutantStack<T>::MutantStack(/* args */)
-{
-}
+// template<typename T>
+// MutantStack<T>::MutantStack(/* args */)
+// {
+// }
 
-template<typename T>
-MutantStack<T>::~MutantStack()
-{
-}
+// template<typename T>
+// MutantStack<T>::~MutantStack()
+// {
+// }
 
 
 #endif
--- CPP08/ex02/src/main.cpp
+++ CPP08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 15:01:52 by akdovlet          #+#    #+#             */
-/*   Updated: 2025/09/29 18:00:37 by akdovlet         ###   ########.fr       */
+/*   Updated: 2025/09/30 18:45:02 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@ int main()
 	MutantStack<int> mt;
 	
 	mt.push(13);
-	std::cout << mt.top() << std::endl;
+	mt.push(10);
+	mt.push(20);
+	mt.push(40);
+
+	MutantStack<int> m2 = mt;
+	for (int i = 0; i < 4; i++)
+	{
+		std::cout << m2.top() << std::endl;
+		m2.pop();
+	}
 	return (0);
 }
