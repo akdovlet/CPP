e94f7347cf00360034b0b0613635f6da30115ef3
akdovlet <akhmed.dovletov@live.fr>
2025-10-28 19:59:24 +0100

    WIP: CPP09/ex02

--- CPP09/ex02/include/GroupIterator.hpp
+++ CPP09/ex02/include/GroupIterator.hpp
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 16:46:16 by akdovlet          #+#    #+#             */
-/*   Updated: 2025/10/26 17:27:56 by akdovlet         ###   ########.fr       */
+/*   Updated: 2025/10/28 18:44:25 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,22 @@
 
 #include <iterator>
 
-template<typename iterator>
-class GroupIterator : public std::iterator<
-										std::random_access_iterator_tag,
-										iterator,
-										std::ptrdiff_t,
-										iterator*,
-										iterator&
-										>
+template<class iterator>
+void	iter_swap(iterator a, iterator b)
+{
+	using std::swap;
+	swap(*a, *b);
+}
+
+template<class iterator>
+void	range_swap(iterator first1, iterator last1, iterator first2)
+{
+	for (; first1 != last1; first1++, first2++)
+		iter_swap<iterator, iterator>(first1, first2);
+}
+
+template<typename iterator, typename value = int>
+class GroupIterator
 {
 private:
 	iterator 		it;
@@ -31,25 +39,39 @@ public:
 	typedef std::random_access_iterator_tag								iterator_category;
 	typedef iterator													iterator_type;
 	typedef typename std::iterator_traits<iterator>::difference_type	iterator_difference;
+	typedef typename std::iterator_traits<iterator>::value_type			value_type;
 	typedef typename std::iterator_traits<iterator>::pointer			iterator_pointer;
 	typedef typename std::iterator_traits<iterator>::reference			iterator_reference;
-	
+
 	GroupIterator() : size(0) {}
 	GroupIterator(iterator it, std::size_t size = 0) : it(it), size(size) {}
 	~GroupIterator() {}
-	
-	void	swap(GroupIterator& a, GroupIterator& b)
-	{
-		using std::swap;
-		swap(a.it, b.it);
-		swap(a.size, b.size);
-	}
-	
-	GroupIterator& operator=(GroupIterator other) { swap(*this, other); return (*this); }
 
-	iterator	getIt() { return (it); }
+	void	swap(GroupIterator& a, GroupIterator& b) { using std::swap;	swap(a.it, b.it); swap(a.size, b.size); }
+
+	iterator	getIterator() { return (it); }
 	std::size_t	getSize() { return (size); }
+
+	value operator*() const { return (it[size - 1]); }
+
+	GroupIterator& operator=(GroupIterator other) { swap(*this, other); return (*this); }
+	GroupIterator& operator++() { it += size; return (*this); }
+	GroupIterator operator++(int) { GroupIterator tmp(*this); operator++(); return (tmp); }
+	GroupIterator& operator--() { it -= size; return (*this); }
+	GroupIterator operator--(int) { GroupIterator tmp(*this); operator--(); return (tmp); }
+
+	GroupIterator& operator+=(iterator_difference increment) { it += size * increment; return (*this); }
+	GroupIterator& operator-=(iterator_difference increment) { it -= size * increment; return (*this); }
+
+	GroupIterator operator+(iterator_difference mov) { iterator oldIt = it; it += mov; GroupIterator tmp(*this); it = oldIt; return (tmp); }
+	GroupIterator operator-(iterator_difference mov) { iterator oldIt = it; it -= mov; GroupIterator tmp(*this); it = oldIt; return (tmp); }
+	
+	value	operator[](std::size_t pos) { return (it[pos * size + size -1]); }
+	value	operator[](std::size_t pos) const { return (it[pos * size + size -1]); }
 	
 };
+
+template<typename iterator>
+void	swap_ranges(GroupIterator<iterator> a, GroupIterator<iterator> b) { range_swap<iterator>(a.getIterator(), a.getIterator() + a.getSize(), b.getIterator()); }
 
 #endif
--- CPP09/ex02/src/main.cpp
+++ CPP09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akdovlet <akdovlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 16:46:52 by akdovlet          #+#    #+#             */
-/*   Updated: 2025/10/26 17:11:30 by akdovlet         ###   ########.fr       */
+/*   Updated: 2025/10/28 19:59:10 by akdovlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 
 int main()
 {
+	std::vector<int> vec;
+	
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.push_back(5);
+	vec.push_back(6);
+	vec.push_back(7);
+	
+	std::vector<int>::iterator it = vec.begin();
+	
+	GroupIterator<std::vector<int>::iterator> groupIt(it, 1);
+
+	swap_ranges<std::vector<int>::iterator >(groupIt, ++groupIt);
+
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++)
+		std::cout << *it << std::endl;
+	
+	
 	return (0);
 }
